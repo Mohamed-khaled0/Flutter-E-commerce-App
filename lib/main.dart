c04e1876395ae5b7da1d9e26457f08c2aaedd487
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-11-06 09:01:00 +0200

    Add scg & flutter gen packages

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class FruitApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
     );
