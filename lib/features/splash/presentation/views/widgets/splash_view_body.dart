cdf2cec750b795dc5ed09d04e20ca00a981a4ee4
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-11-06 09:50:30 +0200

    Add splash view logic

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,21 @@
 import 'package:e_commerce/core/utils/app_images.dart';
+import 'package:e_commerce/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    executeNavigation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,5 +33,11 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(fit: BoxFit.fill, Assets.imagesSplashBottom),
       ],
     );
+  }
+
+  void executeNavigation() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
