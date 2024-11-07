84a3307b03cf0b7a69dd9b7001f50c6600f0cfb9
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-11-07 19:23:02 +0200

    Create auth repo , domain , failure

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,8 @@
+import 'package:e_commerce/core/errors/failure.dart';
+import 'package:e_commerce/features/auth/domain/entites/user_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      String email, String password);
+}
